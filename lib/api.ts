5c5472c2ca7840ecefe1a2e147bcf0b97eeef0f8
nagi <nagireddy2921@gmail.com>
2026-06-05 21:24:28 +0530

    Api update

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL ? `${process.env.NEXT_PUBLIC_API_URL}/api` : 'http://localhost:5001/api',
+    baseURL: 'https://toxnilbackendapp-1.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
